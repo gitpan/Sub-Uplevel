516b3811c27238f697772c14dab03db761d60688
David Golden <dagolden@cpan.org>
2007-10-31 11:02:30 +0000

    Import of DAGOLDEN/Sub-Uplevel-0.18 from CPAN.
    gitpan-cpan-distribution: Sub-Uplevel
    gitpan-cpan-version:      0.18
    gitpan-cpan-path:         DAGOLDEN/Sub-Uplevel-0.18.tar.gz
    gitpan-cpan-author:       DAGOLDEN
    gitpan-cpan-maturity:     released

--- lib/Sub/Uplevel.pm
+++ lib/Sub/Uplevel.pm
@@ -1,10 +1,8 @@
 package Sub::Uplevel;
 
-#use 5.006;
-
 use strict;
 use vars qw($VERSION @ISA @EXPORT);
-$VERSION = '0.17_01';
+$VERSION = '0.18';
 
 # We must override *CORE::GLOBAL::caller if it hasn't already been 
 # overridden or else Perl won't see our local override later.
@@ -20,6 +18,10 @@ require Exporter;
 =head1 NAME
 
 Sub::Uplevel - apparently run a function in a higher stack frame
+
+=head1 VERSION
+
+This documentation describes version 0.18
 
 =head1 SYNOPSIS
 
