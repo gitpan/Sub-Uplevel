8d5abfe4f65ad54b15450168b05b7df5388bf793
David Golden <dagolden@cpan.org>
2012-02-07 19:33:52 +0000

    Import of DAGOLDEN/Sub-Uplevel-0.23_02 from CPAN.
    gitpan-cpan-distribution: Sub-Uplevel
    gitpan-cpan-version:      0.23_02
    gitpan-cpan-path:         DAGOLDEN/Sub-Uplevel-0.23_02.tar.gz
    gitpan-cpan-author:       DAGOLDEN
    gitpan-cpan-maturity:     developer

--- lib/Sub/Uplevel.pm
+++ lib/Sub/Uplevel.pm
@@ -2,7 +2,7 @@ package Sub::Uplevel;
 use 5.006;
 use strict;
 # ABSTRACT: apparently run a function in a higher stack frame
-our $VERSION = '0.23_01'; # VERSION
+our $VERSION = '0.23_02'; # VERSION
 
 # Frame check global constant
 our $CHECK_FRAMES;
@@ -157,7 +157,7 @@ Sub::Uplevel - apparently run a function in a higher stack frame
 
 =head1 VERSION
 
-version 0.23_01
+version 0.23_02
 
 =head1 SYNOPSIS
 
